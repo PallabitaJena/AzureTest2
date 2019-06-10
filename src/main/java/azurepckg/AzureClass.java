0efb0d08bf6dd6df08af8fa8985ca65983e0975e
759791 <759791@PC309253.cts.com>
2019-06-10 16:37:34 +0530

    Second commit

--- src/main/java/azurepckg/AzureClass.java
+++ src/main/java/azurepckg/AzureClass.java
@@ -1,13 +1,19 @@
 package azurepckg;
 
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 public class AzureClass {
-	
+	public SoftAssert softAssert;
+
 	@Test
 	public void method1()
 	{
 		System.out.println("Kuddoooooooos");
+		 softAssert=new SoftAssert();
+		 softAssert.assertEquals("Hello","Hi");
+
+
 	}
 
 }
